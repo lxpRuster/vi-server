0324b07505ac3ced6782592f18d539960e7c563a
lxp <liaoxinpeng@chainsuns.com>
2021-09-22 17:30:18 +0800

    2021/09/22 vercel-test

--- api/router_events/games/queryAllGameNameAndId.js
+++ api/router_events/games/queryAllGameNameAndId.js
@@ -1,11 +1,15 @@
 const mysql = require('../../../config/mysql')
+const { QueryTypes } = require('sequelize');
+const sequelize = require('../../../config/mysql2')
+
 const Result = require('../../../constants/result')
 
 module.exports = async (req, res) => {
 
     const sql = 'select gameId,gameName from NewGame';
 
-    const result = await mysql.query(sql);
+    const result = await sequelize.query(sql, { type: QueryTypes.SELECT });
+    // const result = await mysql.query(sql);
     const data = Result.commonResult(result);
 
     res.status(200).json(data);
