02c412248ccf5515610f939bf40537af298ca1e1
lxp <liaoxinpeng@chainsuns.com>
2021-09-23 09:58:22 +0800

    2021/09/22 vercel-test

--- config/mysql2.js
+++ config/mysql2.js
@@ -6,8 +6,14 @@ const {Sequelize} = require("sequelize");
 //     dialect: 'mysql',
 //     dialectModule: require('mysql2'),
 // });
-const sequelize = new Sequelize('router_events', 'admin', 'admin123456', {
-    host: '139.9.214.55',
+// const sequelize = new Sequelize('router_events', 'admin', 'admin123456', {
+//     host: '139.9.214.55',
+//     port: 3306,
+//     dialect: 'mysql',
+//     dialectModule: require('mysql2'),
+// });
+const sequelize = new Sequelize('router_events', 'nullsw', 'ChXWZmmccsdhiStSI4', {
+    host: 'rm-3nsbo8xy0h258g04yjo.mysql.rds.aliyuncs.com',
     port: 3306,
     dialect: 'mysql',
     dialectModule: require('mysql2'),
