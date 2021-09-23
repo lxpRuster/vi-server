7c6b3118a73f1a88ff0b4aa2ab4f20ff2578f244
lxp <liaoxinpeng@chainsuns.com>
2021-09-23 10:14:25 +0800

    2021/09/22 vercel-test

--- config/mysql2.js
+++ config/mysql2.js
@@ -17,6 +17,13 @@ const sequelize = new Sequelize('router_events', 'nullsw', 'ChXWZmmccsdhiStSI4',
     port: 3306,
     dialect: 'mysql',
     dialectModule: require('mysql2'),
+    pool: {
+        max: 15,
+        min: 5,
+        idle: 20000,
+        evict: 15000,
+        acquire: 30000
+    },
 });
 
 module.exports = sequelize
