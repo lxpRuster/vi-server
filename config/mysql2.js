b3c6775b354d0099562eb483dc84536bf4d8bab1
lxp <liaoxinpeng@chainsuns.com>
2021-09-23 09:51:56 +0800

    2021/09/22 vercel-test

--- config/mysql2.js
+++ config/mysql2.js
@@ -1,7 +1,13 @@
 // Require and initialize outside of your main handler
 const {Sequelize} = require("sequelize");
-const sequelize = new Sequelize('router_events', 'vercel', 'vercel123', {
-    host: 'db4free.net',
+// const sequelize = new Sequelize('router_events', 'vercel', 'vercel123', {
+//     host: 'db4free.net',
+//     port: 3306,
+//     dialect: 'mysql',
+//     dialectModule: require('mysql2'),
+// });
+const sequelize = new Sequelize('router_events', 'admin', 'admin123456', {
+    host: '139.9.214.55',
     port: 3306,
     dialect: 'mysql',
     dialectModule: require('mysql2'),
